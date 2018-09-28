0d37945279042e4e83490dfe4830327791656da7
rach-2 <rache.lowe@btconnect.com>
2018-09-28 15:04:18 +0100

    intail commit

---
+++ dypraxicWorking/src/main/java/com/rachel/dypraxicWorking/DypraxicWorkingApplication.java
@@ -0,0 +1,12 @@
+package com.rachel.dypraxicWorking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DypraxicWorkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DypraxicWorkingApplication.class, args);
+	}
+}
---
+++ dypraxicWorking/src/test/java/com/rachel/dypraxicWorking/DypraxicWorkingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rachel.dypraxicWorking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DypraxicWorkingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
